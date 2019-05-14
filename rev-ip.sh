d685086f07a5b281068765bd64505dc5aaac9fc9
Muhammad Hidayat <49159167+codayid@users.noreply.github.com>
2019-05-14 23:21:01 +0700

    Create rev-ip.sh

---
+++ rev-ip.sh
@@ -0,0 +1,24 @@
+#code by CodayID
+# TEH Squad Cyber # Anon Cyber Team #
+
+#tools
+
+echo "\033[91m
+[-]---------------------------------[-]
+ |   ____    __     __   ___ ____    |
+ |  |  _ \ __\ \   / /  |_ _|  _ \   |
+ |  | |_) / _ \ \ / /____| || |_) |  |
+ |  |  _ <  __/\ V /_____| ||  __/   |
+ |  |_| \_\___| \_/     |___|_|      |
+[-]---------------------------------[-]
+ | TEH SQUAD CYBER + ANON CYBER TEAM |
+ | Code By : CodayID (DAYAT)         |
+[-]---------------------------------[-]"
+echo "\033[1m |                                   |"
+read -p " | WEB YANG INGIN DI REVERSE :" yat;
+echo "[-]---------------------------------[-]"
+echo ""
+echo "PROSES..."
+sleep 2
+echo "\033[91m"
+curl http://api.hackertarget.com/reverseiplookup/?q=$yat  hjj
